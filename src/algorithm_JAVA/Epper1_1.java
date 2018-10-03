de6839f8911c4d9a2640646de4c95034ca61f240
LAPTOP-5LF2RRRC\user <mor2222@naver.com>
2018-10-03 20:43:56 +0900

    2018_10_3 / sopt 알고리즘 스터디

---
+++ src/algorithm_JAVA/Epper1_1.java
@@ -0,0 +1,14 @@
+package algorithm_JAVA;
+
+import java.util.Scanner;
+
+public class Epper1_1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double radius, area;
+		System.out.print("Enter the radius:");
+		radius = in.nextFloat();
+		area = radius*radius*Math.PI;
+		System.out.print("The area is "+area);
+	}
+}
---
+++ src/algorithm_JAVA/SOPT_1.java
@@ -0,0 +1,62 @@
+package algorithm_JAVA;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SOPT_1 {
+	static StringBuilder stringBuilder = new StringBuilder();
+	static  ArrayList<Integer> stack = new ArrayList<>();
+	static int top;
+	public static void push(int num){
+		stack.add(num);
+		top++;
+	}
+
+	public static int pop(){
+		if(top != -1){
+			return stack.remove(top--);
+		}
+		return -1;
+
+	}
+	public  static int isEmpty(){
+		if(stack.size() ==0){
+			return 1;
+		}
+		return 0;
+	}
+	public static int top(){
+		if(top != -1)
+			return stack.get(top);
+		return -1;
+	}
+	public static void main(String args[]) {
+		int input, number;
+		String command;
+		Scanner in = new Scanner(System.in);
+		input = in.nextInt();
+		top = -1;
+		for(int i=0; i<input; i++){
+			command = in.next();
+			switch (command){
+				case "push":
+					number = in.nextInt();
+					push(number);
+					break;
+				case "size":
+					stringBuilder.append(stack.size()).append("\n");
+					break;
+				case "empty":
+					stringBuilder.append(isEmpty()).append("\n");
+					break;
+				case "pop":
+					stringBuilder.append(pop()).append("\n");
+					break;
+				case "top":
+					stringBuilder.append(top()).append("\n");
+					break;
+			}
+		}
+		System.out.print(stringBuilder.toString());
+	}
+}
