54985db3c4fe38dcc31e2ecb1c5264955040d55b
LAPTOP-5LF2RRRC\user <mor2222@naver.com>
2018-11-02 16:31:13 +0900

    2018_11_02_백준 괄호 0912번

---
+++ src/algorithm_JAVA/SOPT_4.java
@@ -0,0 +1,47 @@
+package algorithm_JAVA;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class SOPT_4 {
+
+    public static StringBuilder stringBuilder = new StringBuilder();
+
+
+    public static boolean check(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] word = new char[str.length()];
+        for(int i=0; i<str.length();i++){
+            word[i] = str.charAt(i);
+        }
+        for (int i = 0; i < word.length; i++) {
+
+            if (word[i]=='(')
+                stack.push('(');
+            if (word[i]==')') {
+                if (stack.isEmpty())
+                    return false;
+                stack.pop();
+            }
+        }
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String args[]){
+        int num;
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        for(int i=0; i<num; i++){
+            String str = in.next();
+            if(check(str))
+                stringBuilder.append("YES\n");
+            else
+                stringBuilder.append("NO\n");
+        }
+        System.out.print(stringBuilder);
+    }
+}
