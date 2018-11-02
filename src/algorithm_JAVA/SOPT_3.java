0f8fd94f67e302fca3d970e694e47cfe77290bc8
LAPTOP-5LF2RRRC\user <mor2222@naver.com>
2018-11-02 15:45:49 +0900

    2018_11_02_백준 스택수열 1874번

---
+++ src/algorithm_JAVA/SOPT_3.java
@@ -0,0 +1,34 @@
+package algorithm_JAVA;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SOPT_3 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int arrsize = in.nextInt();
+        int[] arr = new int[arrsize];
+
+        for (int i = 0; i < arrsize; i++) {
+            arr[i] = in.nextInt();
+        }
+        int index = 0;
+        for (int i = 0; i < arrsize; i++) {
+            stack.push(i + 1);
+            stringBuilder.append("+\n");
+            while (!(stack.isEmpty()) && stack.peek() == arr[index]) {
+                stack.pop();
+                stringBuilder.append("-\n");
+                index++;
+            }
+        }
+        if (!stack.isEmpty())
+            System.out.println("NO");
+        else
+            System.out.print(stringBuilder);
+    }
+}
