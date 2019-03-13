39cc630d725636123c57323f90714129b17d4966
mjung1798 <mor2222@naver.com>
2019-03-13 17:04:25 +0900

    hanoi tower complete

---
+++ src/com/jyami/Main_11729.java
@@ -0,0 +1,33 @@
+package com.jyami;
+
+import java.io.*;
+
+public class Main_11729 {
+
+    public static StringBuilder sb = new StringBuilder();
+    public static int count =0;
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(br.readLine());
+
+        hanoi(number, 1,3,2);
+        System.out.println(count);
+        System.out.println(sb.toString());
+
+    }
+
+    public static void hanoi(int size,int from, int to, int by){
+        if(size == 1){
+            sb.append(from+ " " + to+"\n");
+            count++;
+        }else{
+            hanoi(size-1, from, by, to);
+            count++;
+            sb.append(from+ " " + to+"\n");
+            hanoi(size-1, by, to, from);
+        }
+    }
+}
