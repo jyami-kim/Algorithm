0c7c6926468e45e5efdc48cbbf6430cfbdd84d4b
mjung1798 <mor2222@naver.com>
2019-09-26 17:37:50 +0900

    1026 풀이 완료 그치만 의문

--- src/com/jyami/Main_1026.java
+++ src/com/jyami/Main_1026.java
@@ -1,45 +1,39 @@
 package com.jyami;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main_1026 {
 
-    public static BufferedReader bufferedReader;
 
-    public static void main(String args[]) throws IOException {
-        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int result = 0;
+    public static void main(String args[]) {
 
-        int arrSize = bufferedReader.read();
+        Scanner scanner = new Scanner(System.in);
+        int arrSize = scanner.nextInt();
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        int sum = 0;
 
-        List<Integer> a = getArrayFromInput();
-        List<Integer> b = getArrayFromInput();
+        for (int i = 0; i < arrSize; i++) {
+            a.add(scanner.nextInt());
+        }
+
+        for (int i = 0; i < arrSize; i++) {
+            b.add(scanner.nextInt());
+        }
 
         a.sort(Comparator.naturalOrder());
-        a.sort(Comparator.reverseOrder());
+        b.sort(Comparator.reverseOrder());
 
         for (int i = 0; i < arrSize; i++) {
             int mult = a.get(i) * b.get(i);
-            result += mult;
+            sum += mult;
         }
 
-        System.out.println(result);
+        System.out.println(sum);
 
     }
-
-    public static List<Integer> getArrayFromInput() throws IOException {
-        String[] integersInString = bufferedReader.readLine().split(" ");
-        Integer array[] = new Integer[integersInString.length];
-        for (int i = 0; i < integersInString.length; i++) {
-            array[i] = Integer.parseInt(integersInString[i]);
-        }
-        return Arrays.asList(array);
-    }
-
 
 }
