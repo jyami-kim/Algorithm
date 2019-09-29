7d9ca6d389bd8851c768d4df502de6e04f874447
mjung1798 <mor2222@naver.com>
2019-09-29 23:21:56 +0900

    programmers level test 1

---
+++ src/com/jyami/Programmers_LevelTest_1.java
@@ -0,0 +1,46 @@
+package com.jyami;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Programmers_LevelTest_1 {
+
+    public static void main(String args[]) {
+        String [] strings = {"sun", "bed", "car"};
+        int n = 1;
+        Solution solution = new Solution();
+        String[] solution1 = solution.solution(strings, n);
+        for(String s: solution1){
+            System.out.println(s);
+        }
+    }
+
+    static class Solution {
+        public String[] solution(String[] strings, int n) {
+
+            Arrays.sort(strings, new CustomCompartor(n));
+
+            return strings;
+        }
+    }
+
+    static class CustomCompartor implements Comparator<String> {
+
+        int index = 0;
+
+        public CustomCompartor(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.charAt(index) > o2.charAt(index) ? 1 : o1.charAt(index) < o2.charAt(index) ? -1 :
+                    o1.compareTo(o2);
+        }
+    }
+
+
+
+}
+
---
+++ src/com/jyami/Programmers_LevelTest_2.java
@@ -0,0 +1,59 @@
+package com.jyami;
+
+public class Programmers_LevelTest_2 {
+    public static void main(String args[]) {
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+
+        Solution solution = new Solution();
+        int solution1 = solution.solution(n, lost, reserve);
+        System.out.println(solution1);
+
+    }
+
+
+    static class Solution {
+        public int solution(int n, int[] lost, int[] reserve) {
+            int answer = 0;
+
+            int[] result = new int[n + 1];
+
+            for (int i = 0; i < lost.length; i++) {
+                for (int j = 0; j < reserve.length; j++) {
+                    if(lost[i] == reserve[j]){
+                        lost[i] = -1;
+                        reserve[j] = -1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < lost.length; i++) {
+                if(lost[i] == -1){
+                    continue;
+                }
+                result[lost[i]] = 1;
+                answer++;
+            }
+
+            for (int i = 0; i < reserve.length; i++) {
+                int index = reserve[i];
+                if(index == -1){
+                    continue;
+                }
+                if (index != 0 && result[index - 1] == 1) {
+                    result[index - 1] = 0;
+                    answer--;
+                    continue;
+                } else if (index != n && result[index + 1] == 1) {
+                    result[index + 1] = 0;
+                    answer--;
+                    continue;
+                }
+            }
+
+            return n - answer;
+        }
+    }
+
+}
