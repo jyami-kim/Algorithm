fa7209427a45295b85525565eba77b068cc43f1d
mjung1798 <mor2222@naver.com>
2020-01-05 22:09:42 +0900

    프로그래머스 해시 위장

--- algorithm_JAVA/src/com/jyami/custom/Combination.java
+++ algorithm_JAVA/src/com/jyami/custom/Combination.java
@@ -1,8 +1,62 @@
 package com.jyami.custom;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by jyami on 2020/01/05
- * Java에는 왜 조합 라이브러리가 없을까 스스로 구현
+ * Java에는 왜 조합 라이브러리가 없을까 스스로 구현해봄
  */
-public class Combination1 {
+public class Combination {
+
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+        boolean[] visitied = new boolean[4];
+
+        System.out.println("combination backtracking");
+        for(int i =0; i< 4; i++){
+            combinationBackTracking(integers, visitied, 0, 4, i);
+        }
+        System.out.println("===\n");
+
+        System.out.println("combination recursion");
+        for(int i =0; i< 4; i++){
+            combinationRecursion(integers, visitied, 0, 4, i);
+        }
+
+    }
+
+    static void combinationBackTracking(List<Integer> arr, boolean[] visited, int start, int n, int r){
+        if(r == 0){
+            print(arr, visited);
+        } else {
+            for(int i =start; i<n; i++){
+                visited[i] = true;
+                combinationBackTracking(arr, visited, i+1, n, r-1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    static void combinationRecursion(List<Integer> arr, boolean[] visited, int depth, int n, int r){
+        if(r == 0 && depth ==n){
+            print(arr, visited);
+        }
+        if(depth == n){
+            return ;
+        }else{
+            visited[depth] = true;
+            combinationRecursion(arr, visited, depth + 1, n , r-1);
+            visited[depth] = false;
+            combinationRecursion(arr, visited, depth + 1, n , r);
+        }
+    }
+
+    static void print(List<Integer> arr, boolean[] visited){
+        for(int i =0; i < arr.size(); i++){
+            if(visited[i])
+                System.out.print(arr.get(i)+" ");
+        }
+        System.out.println();
+    }
 }
--- algorithm_JAVA/src/com/jyami/programmers/hash/Camouflage.java
+++ algorithm_JAVA/src/com/jyami/programmers/hash/Camouflage.java
@@ -1,7 +1,46 @@
 package com.jyami.programmers.hash;
 
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.*;
+
 /**
  * Created by jyami on 2020/01/05
+ * 위장
+ * https://programmers.co.kr/learn/courses/30/lessons/42578
  */
+
 public class Camouflage {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        int solution1 = solution.solution(clothes);
+        System.out.println(solution1);
+    }
+
+    static class Solution {
+
+        public int solution(String[][] clothes) {
+            int answer = 1;
+            HashMap<String, Integer> map = new HashMap();
+            for(String[] cloth : clothes){
+                int count = map.getOrDefault(cloth[1], 0);
+                map.put(cloth[1], ++count);
+            }
+
+            List<Integer> values =  new ArrayList<>(map.values());
+            for(int value : map.values()){
+                answer *= (value+1);
+            }
+
+            return answer-1;
+        }
+
+        // 팁 순서가 있는 순열과 마찬가지 이기 때문에 가능하다.
+        // 상의 : A가지 // 하의 : B가지 // 모자 : C가지
+        // (A+1)(B+1)(C+1)-1
+        // +1을 하는 이유는 벗는 경우가 있기 때문이다. -1을 하는 이유는 아무것도 안입으면 안되기 때문이다.
+
+    }
 }
