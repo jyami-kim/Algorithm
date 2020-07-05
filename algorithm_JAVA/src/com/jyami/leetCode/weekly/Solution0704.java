80cc0bc4bd76c7a9839b74ebfeb09745ed7650ec
mjung1798 <mor2222@naver.com>
2020-07-05 16:05:08 +0900

    Feat: solution0704 0705

---
+++ algorithm_JAVA/src/com/jyami/leetCode/weekly/Solution0704.java
@@ -0,0 +1,31 @@
+package com.jyami.leetCode.weekly;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by jyami on 2020/07/05
+ */
+public class Solution0704 {
+    public int nthUglyNumber(int n) {
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        int ugly, i2 = 0, i3 = 0, i5 = 0;
+
+        for (int i = 1; i < n; i++) {
+            ugly = Math.min(Math.min(dp[i2] * 2, dp[i3] * 3), dp[i5] * 5);
+            dp[i] = ugly;
+            if (ugly == dp[i2] * 2) ++i2;
+            if (ugly == dp[i3] * 3) ++i3;
+            if (ugly == dp[i5] * 5) ++i5;
+        }
+
+        return dp[n-1];
+    }
+
+    @Test
+    void nthUglyNumberTest() {
+        System.out.println(nthUglyNumber(10));
+    }
+}
