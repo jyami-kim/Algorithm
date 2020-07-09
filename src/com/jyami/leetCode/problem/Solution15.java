4160241f7b454e5beba267f7910c5a9c46e6604d
mjung1798 <mor2222@naver.com>
2020-07-09 10:45:30 +0900

    Leetcode: daily 0708

---
+++ src/com/jyami/leetCode/problem/Solution15.java
@@ -0,0 +1,50 @@
+package com.jyami.leetCode.problem;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+/**
+ * Created by jyami on 2020/07/09
+ */
+public class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+
+        for (int n = 0; n < nums.length - 2; n++) {
+            if (n == 0 || nums[n] != nums[n - 1]) {
+                int sum = -nums[n];
+                int low = n + 1;
+                int high = nums.length - 1;
+                while (low < high) {
+                    if (sum == nums[low] + nums[high]) {
+                        result.add(Arrays.asList(-sum, nums[low], nums[high]));
+                        List<Integer> list = new ArrayList<>();
+                        while ((low < high) && nums[low] == nums[low + 1]) {
+                            low++;
+                        }
+                        while ((low < high) && nums[high] == nums[high - 1]) {
+                            high--;
+                        }
+                        low++;
+                        high--;
+                    } else if (sum > nums[low] + nums[high]) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+
+    @Test
+    void threeSumTest() {
+        int[] input = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> lists = threeSum(input);
+        System.out.println(lists);
+    }
+}
