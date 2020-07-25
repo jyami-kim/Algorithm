eac768ceb376f0104758a2fa5df27e9cdd06b670
mjung1798 <mor2222@naver.com>
2020-07-25 21:34:16 +0900

    LeetCode: challenge 0726 refactor

--- src/com/jyami/leetCode/problem/Solution154.java
+++ src/com/jyami/leetCode/problem/Solution154.java
@@ -7,22 +7,27 @@ import org.junit.jupiter.api.Test;
  */
 public class Solution154 {
     public int findMin(int[] nums) {
-        int min = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (min > nums[i]) {
-                min = nums[i];
-            }
-            if (nums[i - 1] > nums[i]) {
-                break;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else if (nums[mid] < nums[low]) {
+                high = mid;
+                low++;
+            } else {
+                high--;
             }
         }
-        return min;
+        return nums[low];
     }
 
     @Test
     void findMinTest() {
-//        int[] arr = {1, 3, 5};
-        int[] arr = {2,2,2,0,1};
+//        int[] arr = {3, 1};
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+//        int[] arr = {2,2,2,2};
         int min = findMin(arr);
         System.out.println(min);
     }
