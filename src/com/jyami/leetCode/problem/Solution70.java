06f5a5c29fa593a18e81d9e1de2da0c9392e5be0
mjung1798 <mor2222@naver.com>
2020-07-31 17:09:14 +0900

    Leetcode : challenge 0731

---
+++ src/com/jyami/leetCode/problem/Solution70.java
@@ -0,0 +1,16 @@
+package com.jyami.leetCode.problem;
+
+/**
+ * Created by jyami on 2020/07/31
+ */
+public class Solution70 {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
