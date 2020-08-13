9e8b969e2d8df21a511018ad24406934e38dfac5
mjung1798 <mor2222@naver.com>
2020-08-13 16:32:05 +0900

    Leetcode: 0812 challenge second

--- src/com/jyami/leetCode/problem/Solution119.java
+++ src/com/jyami/leetCode/problem/Solution119.java
@@ -4,29 +4,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Created by jyami on 2020/08/12
  */
 public class Solution119 {
     public List<Integer> getRow(int rowIndex) {
-        int[] prev = new int[rowIndex + 1];
-        prev[0] = 1;
-        for (int i = 1; i < prev.length; i++) {
-            int[] next = new int[rowIndex + 1];
-            next[0] = 1;
-            int j;
-            for (j = 1; j < i; j++) {
-                next[j] = prev[j - 1] + prev[j];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= rowIndex + 1; i++) {
+            list.add(0, 1);
+            for (int j = 1; j < i; j++) {
+                list.set(j, list.get(j) + list.get(j + 1));
             }
-            next[j] = 1;
-            prev = next;
         }
-        return Arrays.stream(prev).boxed()
-                .collect(Collectors.toList());
+        return list;
     }
 
     @ParameterizedTest
