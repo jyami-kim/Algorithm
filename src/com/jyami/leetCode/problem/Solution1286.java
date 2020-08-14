85502d587fb12e535f32eb53bf1a25bb10c6e722
mjung1798 <mor2222@naver.com>
2020-08-14 21:23:14 +0900

    Leetcode: 0813 challenge

---
+++ src/com/jyami/leetCode/problem/Solution1286.java
@@ -0,0 +1,57 @@
+package com.jyami.leetCode.problem;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by jyami on 2020/08/13
+ */
+public class Solution1286 {
+    @Test
+    void combinationIteratorTest() {
+        CombinationIterator abc = new CombinationIterator("abc", 2);
+        System.out.println(abc.next());
+        System.out.println(abc.hasNext());
+        System.out.println(abc.next());
+        System.out.println(abc.hasNext());
+        System.out.println(abc.next());
+        System.out.println(abc.hasNext());
+    }
+}
+
+
+class CombinationIterator {
+
+    String characters;
+    Queue<String> queue = new LinkedList<>();
+
+    public CombinationIterator(String characters, int combinationLength) {
+        this.characters = characters;
+        for (int i = 0; i < (1 << characters.length()); i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < characters.length(); j++) {
+                if ((i & (1 << j)) > 0) { // 주어진 i를 한 자리씩 탐색 : i = 2(10), j = 0(1)  => 3(11) : c추가
+                    sb.append(characters.charAt(i));
+                }
+            }
+            if (sb.toString().length() == combinationLength) {
+                queue.add(sb.toString());
+            }
+        }
+
+    }
+
+    public String next() {
+       return queue.poll();
+    }
+
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+
+
+}
